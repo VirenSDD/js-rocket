9da9840d608454774097f63d32dccc34cbdaff2e
Virenazo <virendhanwani@gmail.com>
2017-07-22 14:59:06 +0100

    Fixing some errors

--- js/rocket.js
+++ js/rocket.js
@@ -5,11 +5,12 @@ var changeState = function (state) {
     document.body.className = 'body-state'+state;
     clearInterval(timer);
     countdownNumber = 10;
+    document.getElementById('countdown').innerHTML = countdownNumber;
 
     if (state == 2) {
         timer = setInterval(function(){
-            document.getElementById('countdown').innerHTML = countdownNumber;
             countdownNumber = countdownNumber-1;
+            document.getElementById('countdown').innerHTML = countdownNumber;
              if (countdownNumber<=0){
             changeState(3);
             };
